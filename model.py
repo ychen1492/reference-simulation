d203c830f2b4a8640879f8bd8b88a65a8fcada43
Yuan Chen <y.chen-21@tudelft.nl>
2023-04-05 21:16:02 +0200

    Add first base model and .gitignore file

---
+++ model.py
@@ -0,0 +1,143 @@
+from darts.models.physics.geothermal import Geothermal
+from darts.models.reservoirs.struct_reservoir import StructReservoir
+from darts.models.darts_model import DartsModel
+from darts.models.physics.iapws.iapws_property_vec import _Backward1_T_Ph_vec
+import numpy as np
+
+
+class Model(DartsModel):
+    def __init__(self, total_time, set_nx, set_ny, set_nz, perms, poro,
+                 set_dx, set_dy, set_dz, report_time_step, overburden):
+        # call base class constructor
+        super().__init__()
+
+        self.timer.node["initialization"].start()
+
+        # parameters for the reservoir
+        (nx, ny, nz) = (set_nx, set_ny, set_nz)
+        perms = np.ones(set_nx * set_ny) * perms
+        poro = np.ones(set_nx * set_ny * set_nz) * poro
+        self.perm = np.concatenate([np.ones(nz) * perm for perm in perms], axis=0)
+        self.poro = poro
+        # add more layers above the reservoir
+        nz += overburden
+        overburden_prop = np.ones(set_nx * set_ny * overburden) * 1e-5
+        self.perm = np.concatenate([overburden_prop, self.perm])
+        self.poro = np.concatenate([overburden_prop, self.poro])
+        self.report_time = report_time_step
+        # add more layers above or below the reservoir
+        self.reservoir = StructReservoir(self.timer, nx=nx, ny=ny, nz=nz, dx=set_dx, dy=set_dy, dz=set_dz,
+                                         permx=self.perm, permy=self.perm, permz=self.perm * 0.1, poro=self.poro,
+                                         depth=2300)
+
+        # add larger volumes
+        self.reservoir.set_boundary_volume(yz_minus=1e15, yz_plus=1e15, xz_minus=1e15, xz_plus=1e15)
+        # given the x spacing 3000m, the distance between injection well and boundary is 1200m
+        # well spacing is 1200m
+        # add well's locations
+        injection_well_x = int(1200/set_dx)
+        production_well_x = injection_well_x + int(1200/set_dx)
+        self.iw = [injection_well_x, production_well_x]
+        self.jw = [int(set_ny/2), int(set_ny/2)]
+
+        self.well_index = 100
+
+        # add well
+        self.reservoir.add_well("INJ")
+        # add perforations to the payzone
+        start_index = overburden + 1
+        end_index = nz + 1
+        for n in range(1, 2):
+            self.reservoir.add_perforation(well=self.reservoir.wells[-1], i=self.iw[0], j=self.jw[0], k=n,
+                                           well_index=self.well_index)
+
+        # add well
+        self.reservoir.add_well("PRD")
+        # add perforations to te payzone 
+        for n in range(1, 2):
+            self.reservoir.add_perforation(self.reservoir.wells[-1], i=self.iw[1], j=self.jw[1], k=n,
+                                           well_index=self.well_index)
+
+        self.uniform_pressure = 200
+        self.inj_temperature = 300
+        self.prod_temperature = 350
+
+        # rock heat capacity and rock thermal conduction
+        hcap = np.array(self.reservoir.mesh.heat_capacity, copy=False)
+        rcond = np.array(self.reservoir.mesh.rock_cond, copy=False)
+        hcap[self.perm <= 1e-5] = 400 * 2.5  # volumetric heat capacity: kJ/m3/K
+        hcap[self.perm > 1e-5] = 400 * 2.5
+
+        rcond[self.perm <= 1e-5] = 2.2 * 86.4  # kJ/m/day/K
+        rcond[self.perm > 1e-5] = 3 * 86.4
+
+        self.physics = Geothermal(timer=self.timer, n_points=64, min_p=1, max_p=800,
+                                  min_e=10, max_e=30000, mass_rate=False, cache=False)
+
+        # timestep parameters
+        self.params.first_ts = 1e-3
+        self.params.mult_ts = 8
+        self.params.max_ts = 100
+
+        # nonlinear and linear solver tolerance
+        self.params.tolerance_newton = 1e-4
+        self.params.tolerance_linear = 1e-6
+        self.runtime = total_time
+
+        self.timer.node["initialization"].stop()
+
+    def set_initial_conditions(self):
+        # self.physics.set_nonuniform_initial_conditions(self.reservoir.mesh, pressure_grad=100, temperature_grad=30)
+        self.physics.set_uniform_initial_conditions(self.reservoir.mesh, uniform_pressure=self.uniform_pressure,
+                                                    uniform_temperature=self.prod_temperature)
+
+    def set_boundary_conditions(self):
+        for _, w in enumerate(self.reservoir.wells):
+            if 'I' in w.name:
+                w.control = self.physics.new_mass_rate_water_inj(2500, self.inj_temperature)
+            else:
+                w.control = self.physics.new_mass_rate_water_prod(2500)
+
+    def export_pro_vtk(self, file_name='Results'):
+        X = np.array(self.physics.engine.X, copy=False)
+        nb = self.reservoir.mesh.n_res_blocks
+        temp = _Backward1_T_Ph_vec(X[0:2 * nb:2] / 10, X[1:2 * nb:2] / 18.015)
+        press = X[0:2 * nb:2]
+        peclet_number = np.array(self.physics.engine.pecletNumber[0:nb], copy=False)
+
+        local_cell_data = {'Temperature': temp, 'Pressure': press,
+                           'Perm': self.reservoir.global_data['permx'],
+                           'PecletNumber': peclet_number}
+        self.export_vtk(local_cell_data=local_cell_data)
+
+    def run(self, export_to_vtk=False, file_name='data'):
+        import random
+        # use seed to generate the same random values every run
+        random.seed(3)
+        if export_to_vtk:
+            well_loc = np.zeros(self.reservoir.n)
+            # injection well
+            well_loc[(self.jw[0] - 1) * self.reservoir.nx + self.iw[0] - 1] = -1
+            # production well
+            well_loc[(self.jw[1] - 1) * self.reservoir.nx + self.iw[1] - 1] = 1
+            self.global_data = {'well location': well_loc}
+            self.export_vtk(file_name, global_cell_data=self.global_data)
+
+        # now we start to run for the time report--------------------------------------------------------------
+        time_step = self.report_time
+        even_end = int(self.runtime / time_step) * time_step
+        time_step_arr = np.ones(int(self.runtime / time_step)) * time_step
+        if self.runtime - even_end > 0:
+            time_step_arr = np.append(time_step_arr, self.runtime - even_end)
+
+        self.export_pro_vtk(file_name)
+        for ts in time_step_arr:
+            for _, w in enumerate(self.reservoir.wells):
+                if 'I' in w.name:
+                    w.control = self.physics.new_mass_rate_water_inj(2500, self.inj_temperature)
+                else:
+                    w.control = self.physics.new_mass_rate_water_prod(2500)
+            self.physics.engine.run(ts)
+            self.physics.engine.report()
+            if export_to_vtk:
+                self.export_pro_vtk(file_name)
---
+++ real_base.py
@@ -0,0 +1,49 @@
+import os
+
+import numpy as np
+import pandas as pd
+from darts.engines import redirect_darts_output
+
+from model import Model
+
+report_time = 100
+total_time = 10000
+perms = 3000
+poro = 0.2
+
+np.random.seed(550)
+x_spacing = 3000
+y_spacing = 2500
+z_spacing = 100
+set_dx = 18.5
+set_nx = int(x_spacing / set_dx)
+set_dy = 27.5
+set_ny = int(y_spacing / set_dy)
+set_dz = 10
+set_nz = int(z_spacing / set_dz)
+
+
+def generate_base():
+    redirect_darts_output('log.txt')
+    proxy_model = Model(total_time=total_time, set_nx=set_nx, set_ny=set_ny, set_nz=set_nz, set_dx=set_dx,
+                        set_dy=set_dy, set_dz=set_dz, perms=perms, poro=poro, report_time_step=report_time, overburden=0)
+    proxy_model.init()
+    proxy_model.run(export_to_vtk=True)
+
+    proxy_model_elapsed_time = proxy_model.timer.node['initialization'].get_timer() + proxy_model.timer.node[
+        'simulation'].get_timer()
+
+    td = pd.DataFrame.from_dict(proxy_model.physics.engine.time_data)
+
+    if not os.path.exists('RealBase'):
+        os.mkdir('RealBase')
+    output_path = os.path.relpath(f'./RealBase/base_resolution_ho_mass.xlsx')
+    writer = pd.ExcelWriter(output_path)
+    td.to_excel(writer, 'Sheet1')
+    writer.save()
+    with open('./RealBase/simulation_time_resolution_ho_mass.txt', 'w') as f:
+        f.write(f'{proxy_model_elapsed_time}')
+
+
+if __name__ == '__main__':
+    generate_base()
